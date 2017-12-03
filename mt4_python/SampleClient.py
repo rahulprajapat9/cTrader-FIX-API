008d5f78511a84bb9c85f6f83c3307099731a06c
Rahul Prajapat <rahul.prajapat@proemion.com>
2017-12-03 20:12:29 +0100

    simple server client tcp code

--- mt4_python/SampleClient.py
+++ mt4_python/SampleClient.py
@@ -3,15 +3,17 @@
 #   Connects REQ socket to tcp://localhost:5559
 #   Sends "Hello" to server, expects "World" back
 #
-import zmq
+import zmq, time
 
 #  Prepare our context and sockets
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5559")
+socket.connect("tcp://192.168.0.115:5555")
 
 #  Do 10 requests, waiting each time for a response
 for request in range(1,11):
     socket.send(b"Hello")
+    print 'msg sent, recv waiting ...'
     message = socket.recv()
     print("Received reply %s [%s]" % (request, message))
+    time.sleep(10)
