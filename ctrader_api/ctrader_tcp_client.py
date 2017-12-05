70ae6e71f1d716e6793678f7400ffa3ff1ba56fc
Rahul Prajapat <rahul.prajapat@proemion.com>
2017-12-05 02:59:46 +0100

    cTrader logon msg and MarketReq functions done

--- ctrader_api/ctrader_tcp_client.py
+++ ctrader_api/ctrader_tcp_client.py
@@ -1,7 +1,7 @@
 import socket, time
 from datetime import datetime
 
-def ConstructHeader(SenderCompID, TargetCompID, SenderSubID, TargetSubID, messageSequenceNumber):
+def ConstructHeader(msg_type, SenderCompID, TargetCompID, SenderSubID, TargetSubID, messageSequenceNumber):
     header = ''
 
     # Version of FIX
@@ -10,7 +10,7 @@ def ConstructHeader(SenderCompID, TargetCompID, SenderSubID, TargetSubID, messag
     message = ''
 
     # Type of message, A = String
-    message += "35=A|"
+    message += "35=" + msg_type + "|"
 
     message += "49="  + SenderCompID +  "|"
     message += "56="  + TargetCompID +  "|"
@@ -19,12 +19,28 @@ def ConstructHeader(SenderCompID, TargetCompID, SenderSubID, TargetSubID, messag
     message += "57="  + TargetSubID +  "|"
 
     message += "34="  + messageSequenceNumber +  "|"
-    message += "52="  + str(datetime.today()) +  "|"
+
+    utctimenow_year = str(datetime.utcnow().year)
+    utctimenow_month = str(datetime.utcnow().month)
+    utctimenow_date = str(datetime.utcnow().day)
+    utctimenow_date = utctimenow_date.zfill(2)
+
+    utctimenow_hour = str(datetime.utcnow().hour)
+    utctimenow_hour = utctimenow_hour.zfill(2)
+
+    utctimenow_min = str(datetime.utcnow().minute)
+    utctimenow_min = utctimenow_min.zfill(2)
+
+    utctime = utctimenow_year+utctimenow_month+utctimenow_date+'-'+utctimenow_hour+':'+utctimenow_min+':00'
+
+    message += "52="  + utctime +  "|"
+
+    #message += "52=20171204-16:20:00|"
 
     return header, message
 
 def ConstructTrailer(headerAndBody):
-    headerAndBody = str(headerAndBody.replace('|', '\u0001'))
+    headerAndBody = str(headerAndBody.replace('|', chr(0x01)))
     chksum = str(reduce(lambda x,y:x+y, map(ord, headerAndBody)) % 256)
     chksum = chksum.zfill(3)
 
@@ -51,6 +67,8 @@ def LogonMessage(heartBeatSeconds, username, password, resetSeqNum):
 def LogonPacket():
 
     # Header
+    msg_type = 'A'
+
     SenderCompID = 'pepperstone.3224363'
     TargetCompID = 'CSERVER'
 
@@ -59,9 +77,7 @@ def LogonPacket():
     TargetSubID = 'QUOTE'
     messageSequenceNumber = '1'
 
-    header, message = ConstructHeader(SenderCompID, TargetCompID, SenderSubID, TargetSubID, messageSequenceNumber)
-    print 'Header construction completed'
-    print 'Header is: ', header, '\n'
+    header, message = ConstructHeader(msg_type, SenderCompID, TargetCompID, SenderSubID, TargetSubID, messageSequenceNumber)
 
     # Body
     heartBeatSeconds = '30'
@@ -70,25 +86,79 @@ def LogonPacket():
     resetSeqNum = 1
 
     body = LogonMessage(heartBeatSeconds, username, password, resetSeqNum)
-    print 'Body construction completed'
-    print 'Body message is: ', body, '\n'
 
     length = len(message) + len(body)
-    headerAndBody = header + message + body
-    print 'Length of Messasge + Body is: ', length, '\n'
-    headerAndBody += "9=" + str(length) + "|"
+    # FIX version + len of msg
+    header += "9=" + str(length) + "|"
+
+    # FIX version + len of msg + rest of the header
+    header = header + message
+
+    print 'Header construction completed'
+    print 'Header is: ', header, '\n'
+
+    # FIX version + len of msg + rest of the header + body (login)
+    headerAndBody = header + body
 
     #headerAndBody = '8=FIX.4.4|9=126|35=A|49=theBroker.12345|56=CSERVER|34=1|52=20170117-08:03:04|57=TRADE|50=any_string|98=0|108=30|141=Y|553=12345|554=passw0rd!|'
 
     # Trailer
     chksum = ConstructTrailer(headerAndBody)
     trailer = "10="+str(chksum)+"|"
+    #trailer = "10=254|"
+    # 254 QUOTE, 224 TRADE
 
     print 'Trailer construction completed'
     print 'Trailer is: ', trailer, '\n'
 
     headerAndBodyAndTrailer = headerAndBody + trailer
     print 'ctrader packet is: ', headerAndBodyAndTrailer, '\n'
+
+    headerAndBodyAndTrailer = headerAndBodyAndTrailer.replace("|", chr(0x01))
+
+    # ctrader packet constructor finished
+
+    return headerAndBodyAndTrailer
+
+def MarketDataReq(seq_num, subscription_id):
+
+    # Header
+    msg_type = 'V'
+
+    SenderCompID = 'pepperstone.3224363'
+    TargetCompID = 'CSERVER'
+
+    #SenderSubID = 'QUOTE'
+    SenderSubID = 'any_string'
+    TargetSubID = 'QUOTE'
+    messageSequenceNumber = seq_num
+
+    header, message = ConstructHeader(msg_type, SenderCompID, TargetCompID, SenderSubID, TargetSubID, messageSequenceNumber)
+
+    # Body
+    body = "262="+subscription_id+"|263=1|264=1|265=1|146=1|55=41|267=2|269=0|269=1|"
+
+    length = len(message) + len(body)
+    # FIX version + len of msg
+    header += "9=" + str(length) + "|"
+
+    # FIX version + len of msg + rest of the header
+    header = header + message
+
+    # FIX version + len of msg + rest of the header + body (login)
+    headerAndBody = header + body
+
+    #headerAndBody = '8=FIX.4.4|9=126|35=A|49=theBroker.12345|56=CSERVER|34=1|52=20170117-08:03:04|57=TRADE|50=any_string|98=0|108=30|141=Y|553=12345|554=passw0rd!|'
+
+    # Trailer
+    chksum = ConstructTrailer(headerAndBody)
+    trailer = "10="+str(chksum)+"|"
+    #trailer = "10=254|"
+    # 254 QUOTE, 224 TRADE
+
+    headerAndBodyAndTrailer = headerAndBody + trailer
+
+    headerAndBodyAndTrailer = headerAndBodyAndTrailer.replace("|", chr(0x01))
 
     # ctrader packet constructor finished
 
@@ -98,24 +168,43 @@ def Main():
     host = 'h58.p.ctrader.com'
     port = 5201
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket()
     s.connect((host, port))
 
-    print 'Connection to Ctrader API successful\n'
-
-    # Constructing ctrader packet
+    # Logon
+    seq_num = 1
     headerAndBodyAndTrailer = LogonPacket()
+    print 'LOGON packet sent to ctrader!!!'
+    s.send(headerAndBodyAndTrailer)
 
-    # Debug
-    #headerAndBodyAndTrailer = '8=FIX.4.4|9=126|35=A|49=theBroker.12345|56=CSERVER|34=1|52=20170117-08:03:04|57=TRADE|50=any_string|98=0|108=30|141=Y|553=12345|554=passw0rd!|10=131|'
-    #print headerAndBodyAndTrailer
+    data = s.recv(1024)
+    data = str(data)
+    a = data[9]
+    data = data.replace(a, '|')
+    print '\n\nLOGON RESP ', seq_num, '  :  ', data
 
-    while headerAndBodyAndTrailer!='q':
+    subscription_id = 876316403
+    seq_num = 2
+    while(1):
+        # Market Req Data
+        headerAndBodyAndTrailer = MarketDataReq(str(seq_num), str(subscription_id))
+        print 'MARTKET_DATA_REQ packet sent to ctrader!!!'
         s.send(headerAndBodyAndTrailer)
-        print 'packet sent to ctrader!!!'
-        print 'waiting to receive response from ctrader...'
+
         data = s.recv(1024)
-        print 'Received from ctrader: ', str(data)
+        data = str(data)
+        a = data[9]
+        data = data.replace(a, '|')
+        print '\n\nXAUUSD: ', seq_num, '  :  ', data
+
+        if seq_num%2 :
+            subscription_id += 100
+
+        seq_num += 1
+
+        time.sleep(1)
+
+    time.sleep(1000)
 
     s.close()
 
